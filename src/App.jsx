95aa4bd23c471740dbb6c05730494a98c0cff90a
Felipe Siqueira <felipesgomes22@gmail.com>
2022-06-15 11:28:33 -0300

    First component; Global Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import "./global.css"
 
 function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Header />
+
+      <Post 
+        author="Felipe Siqueira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quasi earum laudantium quas voluptatum consequatur harum praesentium ipsam nulla labore fugit quisquam, voluptas aliquam, maiores aperiam amet vitae minus! Dolorum."
+      />
+
+      <Post 
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis eaque obcaecati ad quae maiores. Temporibus dolor unde facilis esse laborum quod nihil, hic necessitatibus reprehenderit quaerat harum distinctio aspernatur autem?"
+      />
     </>
   )
 }
