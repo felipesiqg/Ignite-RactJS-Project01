074f3a2535a6e74eacf795f3cba946e94e3ed2ca
Felipe Siqueira <felipesgomes22@gmail.com>
2022-06-30 10:52:12 -0300

    Create Avatar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,9 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar}
-                    src="https://github.com/felipesiqg.png" />
+                <Avatar src="https://github.com/felipesiqg.png" />
 
                 <strong>Felipe Siqueira</strong>
                 <span>Web Developer</span>
